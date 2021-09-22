b46a78ac733e46ba761871397a8a51c616a5691b
黄高峰 <15879831437@163.com>
2021-09-22 18:11:47 +0800

    添加第三章组件介绍示例

---
+++ ch03/ch03-react-component.js
@@ -0,0 +1,9 @@
+fuction reactComponent () {
+	return <tagName>Hello, React Component</tagName>;
+}
+
+class reactComponent2 extends React.Component {
+	render () {
+		return <tagName>Hello, React Component.<tagName>
+	}
+}
