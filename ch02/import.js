0fd824cdabade2e000d39789687be01b2d0c881d
黄高峰 <15879831437@163.com>
2021-09-22 17:42:34 +0800

    添加表达式

---
+++ ch02/import.js
@@ -0,0 +1,8 @@
+var divReact = document.getElementByI('id-div-react');
+const reactSpan = (
+	<span>
+		<h3>React JSX</h3>
+		<p>Create React DOM by React JSX.</p>
+	</span>
+);
+ReactDOM.render(reactSpan, divReact);
