53297df800c165748c1d8f24cb333f5f38f28ad7
黄高峰 <15879831437@163.com>
2021-09-23 17:59:31 +0800

    更新第4章

---
+++ ch04/react-state.js
@@ -0,0 +1,38 @@
+class ClassName extends React.Component {
+	constructor () {
+
+	}
+	render () {
+		return ();
+	}
+}
+
+class Clock extends React.Component {
+	constructor () {
+
+	}
+	render () {
+		return (
+			<tagName>{this.state...}</tagName>
+		)
+	}
+}
+
+class Clock extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {params: value};
+	}
+	render () {
+		return (
+			<tagName>
+				{this.state...}
+			</tagName>
+		);
+	}
+}
+
+
+
+
+
