ec1582806ee8c0b96e6b7bd78360b5d15657feb1
黄高峰 <15879831437@163.com>
2021-09-23 11:51:05 +0800

    更新第三章示例

--- ch03/ch03-react-component.js
+++ ch03/ch03-react-component.js
@@ -1,9 +1,10 @@
-fuction reactComponent () {
-	return <tagName>Hello, React Component</tagName>;
+function reactComponent () {
+	return <tagName>Hello, React Component.</tagName>;
 }
 
 class reactComponent2 extends React.Component {
 	render () {
-		return <tagName>Hello, React Component.<tagName>
+		return <tagName>Hello, React Component.<tagName>;
 	}
 }
+
